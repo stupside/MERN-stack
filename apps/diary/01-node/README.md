5cfe7030a34bab53a5a4821c86f6ce8b61a565b5
xonery <kilian.houpeurt@gmail.com>
2025-09-03 13:24:30 +0300

    feat(01-node): conditional responses in handler for minimal routing

---
+++ apps/diary/01-node/README.md
@@ -0,0 +1,9 @@
+# 3 September 2025
+
+**01-node is based on the crash course at https://www.youtube.com/watch?v=32M1al-Y6Ag**
+
+1. Created the project with a minimal hello world main file.
+2. Added a node http server that returns Hello in response to requests.
+3. Added configurable PORT of the server that was hardcoded to be 3000.
+4. Instead of returning Hello the API now returns values from the req param.
+5. Added conditional responses in the http server handler for minimal routing.
--- apps/diary/01-node/main.ts
+++ apps/diary/01-node/main.ts
@@ -9,14 +9,19 @@ config({
 const PORT = process.env.PORT
 
 const server = createServer((req, res) => {
-    res.setHeader("Content-Type", "application/json")
 
-    res.end(JSON.stringify({
-        message: "Hello!", req: {
-            url: req.url,
-            method: req.method,
-        }
-    }))
+    if (req.url === "/") {
+        res.setHeader("Content-Type", "text/plain")
+        res.end("Welcome to the Diary API")
+    } else {
+        res.setHeader("Content-Type", "application/json")
+        res.end(JSON.stringify({
+            message: "Hello!", req: {
+                url: req.url,
+                method: req.method,
+            }
+        }))
+    }
 })
 
 server.listen(PORT, () => {
