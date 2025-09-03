e13e16aefb871d76837f31e37be063216903ced9
xonery <kilian.houpeurt@gmail.com>
2025-09-03 20:39:04 +0300

    feat(03-express): setup express server and env

--- apps/diary/03-express/main.ts
+++ apps/diary/03-express/main.ts
@@ -1,1 +1,7 @@
-console.log("Hello!")
+import express from "express"
+
+const app = express()
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running at http://localhost:${process.env.PORT}`)
+})
